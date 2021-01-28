c0c7e570e4150ace32e0561225056be4f4244d3f
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-28 20:53:27 +0100

    Add basic ingredient form

--- recipe/models.py
+++ recipe/models.py
@@ -1,13 +1,19 @@
 from django.db import models
-
+from django.forms import ModelForm
 
 class Ingredient(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=127)
     value = models.FloatField()
-    unit_type = models.TextField()
+    unit_type = models.CharField(max_length=32)
 
 
 class Recipe(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=127)
     ingredients = models.ManyToManyField(Ingredient)
     image = models.ImageField()
+
+
+class IngredientForm(ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'value', 'unit_type']
--- recipe/views.py
+++ recipe/views.py
@@ -1,13 +1,23 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from .models import IngredientForm
 
 def index(request):
     return render(request, "core\\recipes.html")
 
 
 def create_ingredient(request):
-    return render(request, "core\\createingredient.html")
+
+    if request.method == "POST":
+        form = IngredientForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect("create")
+    else:
+        form = IngredientForm()
+
+    return render(request, "core\\createingredient.html", {"form": form})
 
 
 def create_recipe(request):
