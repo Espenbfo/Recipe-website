cd5d572ecdeb26346dc6cc1f0edb07397ffabb79
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-29 09:26:14 +0100

    Tweak create recipe form

--- recipe/models.py
+++ recipe/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.forms import ModelForm
-
-
+from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class IngredientType(models.Model):
-    ingredient_name = models.CharField(max_length=127)
+    ingredient_name = models.CharField(max_length=127, unique=True)
     unit_type = models.CharField(max_length=32)
 
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=127)
     image = models.ImageField(upload_to='static/img/')
+    recipe_description = models.TextField()
 
 class Ingredient(models.Model):
     ingredient_type = models.ForeignKey(IngredientType, on_delete=models.CASCADE)
@@ -23,6 +24,15 @@ class IngredientForm(ModelForm):
         fields = ['ingredient_name', 'unit_type']
 
 class RecipeForm(ModelForm):
+    recipe_name = forms.CharField(label="", max_length=127,
+                                         required=True, widget=forms.TextInput(
+            attrs={'placeholder': 'Tittel'}))
+    recipe_description = forms.CharField(label="", widget=forms.Textarea(
+        attrs={"placeholder": "beskrivelse"}
+    ))
     class Meta:
         model = Recipe
-        fields = ['recipe_name', 'image']
+        fields = ['recipe_name', "recipe_description", 'image']
+        widgets = {
+            "recipe_description": forms.Textarea(attrs={"cols": 30, "rows": 20}),
+        }
--- recipe/views.py
+++ recipe/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-from .models import IngredientForm, IngredientType, Ingredient, Recipe, RecipeForm
+from .models import IngredientForm, IngredientType, Ingredient, Recipe, \
+    RecipeForm
+
 
 def index(request):
     recipes = Recipe.objects.all()
@@ -12,53 +14,63 @@ def index(request):
 
 def ingredients(request):
     ingredients = IngredientType.objects.all()
-    return render(request, "core\\ingredients.html", {"ingredients": ingredients})
+    return render(request, "core\\ingredients.html",
+                  {"ingredients": ingredients})
 
 
 def create_ingredient(request):
-
     if request.method == "POST":
         form = IngredientForm(request.POST)
 
         if form.is_valid():
             name = form.cleaned_data["ingredient_name"]
             unit_type = form.cleaned_data["unit_type"]
-            IngredientType.objects.create(ingredient_name=name,unit_type=unit_type)
+            IngredientType.objects.create(ingredient_name=name,
+                                          unit_type=unit_type)
             return HttpResponseRedirect("create")
     else:
         form = IngredientForm()
 
     return render(request, "core\\createingredient.html", {"form": form})
 
+
 def handle_uploaded_file(f):
     with open('some/file/name.txt', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+
 
 def create_recipe(request):
     if request.method == "POST":
         form = request.POST
 
         name = form["recipe_name"]
+        description = form["recipe_description"]
         print(request.FILES)
         image = request.FILES["image"]
         print(image)
-        r = Recipe.objects.create(recipe_name=name,image=image)
-        #with open("image.jpg", "w") as f:
+        r = Recipe.objects.create(recipe_name=name,
+                                  recipe_description=description, image=image)
+        # with open("image.jpg", "w") as f:
         #    for chunk in image.chunks():
         #        f.write(image)
         print(r)
         ingredient_list = []
         number_of_ingredients = form["number-of-ingredients"]
 
-        for i in range(1,int(number_of_ingredients)+1):
+        for i in range(1, int(number_of_ingredients) + 1):
             name = form[str(i) + "name"]
             value = form[str(i) + "number"]
             ingredient_type = IngredientType.objects.get(ingredient_name=name)
-            ingredient_list.append(Ingredient.objects.create(value=float(value), ingredient_type=ingredient_type, recipe=r))
+            ingredient_list.append \
+                (Ingredient.objects.create(value=float(value),
+                                           ingredient_type=ingredient_type,
+                                           recipe=r))
         return HttpResponseRedirect("")
     else:
         form = RecipeForm()
 
     ingredients = IngredientType.objects.all()
-    return render(request, "core\\createrecipe.html", {"form": form, "ingredients": ingredients})
+    return render(request, "core\\createrecipe.html",
+                  {"form": form,
+                   "ingredients": ingredients})
