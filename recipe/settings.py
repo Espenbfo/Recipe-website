974004fac9a851a3d9eeefd276b9fc7cbfdaaf66
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-30 23:23:57 +0100

    Set debug to true for testing

--- recipe/settings.py
+++ recipe/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["secret-key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.2.31', "127.0.0.1"]
 
