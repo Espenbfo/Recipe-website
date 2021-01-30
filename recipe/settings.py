f91148fcbab18a07d222c7d29f964d3b9930e507
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-30 23:58:26 +0100

    Remove static files

--- recipe/settings.py
+++ recipe/settings.py
@@ -40,8 +40,8 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE ="recipe.storage_backends.MediaStorage"
 
