47eb92224aa95f810c7b487ddaa6f3f5f50797c3
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-31 00:02:47 +0100

    Update settings allow heroku

--- recipe/settings.py
+++ recipe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("secret-key","1")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.2.31', "127.0.0.1"]
+ALLOWED_HOSTS = ['192.168.2.31', "127.0.0.1", "recipe-espen.herokuapp.com"]
 
 AWS_ACCESS_KEY_ID = os.environ.get("access-key","1")
 
